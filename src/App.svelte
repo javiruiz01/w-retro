5bc30dbfb90e87c465a0e7a4bccabca958f2e20f
Javier Ruiz <j.ruiz@payvision.com>
2020-03-21 16:13:04 +0100

    Create card component, and dinamically render it

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,36 @@
 <script>
   import Tailwindcss from './Tailwindcss.svelte';
   import Navbar from './Navbar.svelte';
+  import Card from './Card.svelte';
+
+  const cards = [
+    {
+      title: 'Good',
+      elements: [{ text: 'So much work!' }],
+    },
+    {
+      title: 'Meh',
+      elements: [{ text: 'Innovation week' }],
+    },
+    {
+      title: 'Bad',
+      elements: [{ text: 'Something about infra' }],
+    },
+  ];
+
+  $: getMargin = (idx) => (idx === cards.length - 1 ? 'mr-0' : 'mr-8');
 </script>
 
 <Navbar />
 <main class="main h-full p-12">
   <div class="flex flex-col h-full">
     <span class="text-lg text-gray-700 mb-4">Sprint 6 retro</span>
-    <div
-      class="h-full flex flex-col p-8 bg-white rounded-lg border-gray-400 flex
-      align-center max-w-sm w-full">
-      <div class="w-full text-gray-700 mb-2">Good</div>
-      <div class="w-full">
-        <button
-          class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4
-          rounded h-auto w-full flex justify-center">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-            class="w-4 h-4">
-            <path d="M12 4v16m8-8H4" />
-          </svg>
-        </button>
-      </div>
+    <div class="flex h-full overflow-x-scroll scrollable-container">
+      {#each cards as card, idx}
+        <div class="w-full min-w-1/4 {getMargin(idx)}">
+          <Card data={card} />
+        </div>
+      {/each}
     </div>
   </div>
 </main>
