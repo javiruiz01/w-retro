23b1250b8abbeff54bfa20054c595d29e48fbcd7
Javier Ruiz <j.ruiz@payvision.com>
2020-03-21 19:40:25 +0100

    Allow user to input text

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Tailwindcss from './Tailwindcss.svelte';
   import Navbar from './Navbar.svelte';
 
-  let commentText;
+  let commentText = '';
   let textareaToShow;
   const cards = [
     {
@@ -21,13 +21,16 @@
 
   $: getMargin = (idx) => (idx === cards.length - 1 ? 'mr-0' : 'mr-8');
 
-  function showTextarea(idx) {
+  function toggleTextarea(idx) {
     textareaToShow = textareaToShow === idx ? '' : idx;
   }
 
   function addComment(idx) {
+    if (!commentText.trim()) return;
+
     const comments = cards[idx].comments;
-    cards[idx].comments = [...comments, { text: 'holiwis' }];
+    cards[idx].comments = [...comments, { text: commentText }];
+    commentText = '';
   }
 </script>
 
@@ -46,7 +49,7 @@
               <div class="w-full text-gray-700 mb-2">{title}</div>
               <div class="w-full">
                 <button
-                  on:click={() => showTextarea(idx)}
+                  on:click={() => toggleTextarea(idx)}
                   class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold
                   py-2 px-4 rounded h-auto w-full flex justify-center">
                   <svg
@@ -61,13 +64,46 @@
                   </svg>
                 </button>
               </div>
+              <div class="relative" class:hidden={textareaToShow !== idx}>
+                <textarea
+                  bind:value={commentText}
+                  placeholder="Write something!"
+                  class="mt-2 resize-none border border-solid border-gray-400
+                  rounded focus:outline focuse:shadow-outline w-full py-2 px-4
+                  scrollable-container" />
+                <button
+                  class="absolute top-0 right-0 mt-3 mr-1"
+                  on:click={() => toggleTextarea(idx)}>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    class="w-4 h-4 stroke-current text-red-500 cursor-pointer">
+                    <path d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+                <button
+                  class="absolute bottom-0 right-0 mb-3 mr-1"
+                  on:click={() => addComment(idx)}>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    class=" w-4 h-4 stroke-current text-green-500 cursor-pointer">
+                    <path
+                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118
+                      0z" />
+                  </svg>
+                </button>
+              </div>
             </div>
-            <textarea
-              class:hidden={textareaToShow !== idx}
-              bind:value={commentText}
-              placeholder="Write something!"
-              class="mt-2 resize-none border border-solid border-gray-400
-              rounded focus:outline focuse:shadow-outline w-full py-2 px-4" />
+
             <div class="mt-2">
               {#each comments as element}
                 <div
