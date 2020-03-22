229d3313b95d3a2ea890df8813d9ded3ddced6a5
Javier Ruiz <j.ruiz@payvision.com>
2020-03-22 11:28:44 +0100

    Correctly set overflow x

--- src/App.svelte
+++ src/App.svelte
@@ -23,6 +23,7 @@
 
   function toggleTextarea(idx) {
     textareaToShow = textareaToShow === idx ? '' : idx;
+    commentText = '';
   }
 
   function addComment(idx) {
@@ -38,84 +39,99 @@
 <main class="main h-full p-12">
   <div class="flex flex-col h-full">
     <span class="text-lg text-gray-700 mb-4">Sprint 6 retro</span>
-    <div class="flex h-full overflow-x-scroll scrollable-container">
-      {#each cards as { title, comments }, idx}
-        <div class="w-full min-w-13 {getMargin(idx)}">
-          <div
-            class="h-full flex flex-col py-8 pl-8 pr-6 bg-white rounded-lg
-            border-gray-400 flex align-center w-full shadow-md overflow-y-scroll
-            scrollable-container">
-            <div class="sticky bg-white -top-2">
-              <div class="w-full text-gray-700 mb-2">{title}</div>
-              <div class="w-full mb-2">
-                <button
-                  on:click={() => toggleTextarea(idx)}
-                  class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold
-                  py-2 px-4 rounded h-auto w-full flex justify-center">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4">
-                    <path d="M12 4v16m8-8H4" />
-                  </svg>
-                </button>
-              </div>
-              <div class="relative" class:hidden={textareaToShow !== idx}>
-                <textarea
-                  bind:value={commentText}
-                  placeholder="Write something!"
-                  class="block resize-none border border-solid border-gray-400
-                  rounded focus:outline focuse:shadow-outline w-full py-2 px-4
-                  scrollable-container" />
-                <button
-                  class="absolute top-0 right-0 mt-1 mr-1"
-                  on:click={() => toggleTextarea(idx)}>
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4 stroke-current text-red-500 cursor-pointer">
-                    <path d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-                <button
-                  class="absolute bottom-0 right-0 mb-1 mr-1"
-                  on:click={() => addComment(idx)}>
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    class=" w-4 h-4 stroke-current text-green-500 cursor-pointer">
-                    <path
-                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118
-                      0z" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-
-            <div class="mt-2">
-              {#each comments as element}
-                <div
-                  class="mt-2 text-gray-800 py-2 px-4 rounded h-auto w-full flex
-                  items-center border border-solid border-gray-400">
-                  {element.text}
+    <div class="overflow-x-scroll scrollable-container h-full pb-2 px-2">
+      <div class="flex h-full">
+        {#each cards as { title, comments }, idx}
+          <div class="w-full min-w-13 {getMargin(idx)}">
+            <div
+              class="h-full flex flex-col py-8 pl-8 pr-6 bg-white rounded-lg
+              border-gray-400 flex align-center w-full shadow-md
+              overflow-y-scroll scrollable-container">
+              <div class="sticky bg-white -top-2">
+                <div class="w-full text-gray-700 mb-2">{title}</div>
+                <div class="w-full mb-2">
+                  <button
+                    on:click={() => toggleTextarea(idx)}
+                    class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold
+                    py-2 px-4 rounded h-auto w-full flex justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      class="w-4 h-4">
+                      <path d="M12 4v16m8-8H4" />
+                    </svg>
+                  </button>
                 </div>
-              {/each}
+                <div class="relative" class:hidden={textareaToShow !== idx}>
+                  <textarea
+                    bind:value={commentText}
+                    placeholder="Write something!"
+                    class="block resize-none border border-solid border-gray-400
+                    rounded focus:outline focuse:shadow-outline w-full py-2 px-4
+                    scrollable-container" />
+                  <button
+                    class="absolute top-0 right-0 mt-1 mr-1"
+                    on:click={() => toggleTextarea(idx)}>
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      class="w-4 h-4 stroke-current text-red-500 cursor-pointer">
+                      <path d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                  <button
+                    class="absolute bottom-0 right-0 mb-1 mr-1"
+                    on:click={() => addComment(idx)}>
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      class=" w-4 h-4 stroke-current text-green-500
+                      cursor-pointer">
+                      <path
+                        d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0
+                        0118 0z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div class="mt-2">
+                {#each comments as element}
+                  <div
+                    class="mt-2 text-gray-800 py-2 px-4 rounded h-auto w-full
+                    flex items-center border border-solid border-gray-400">
+                    {element.text}
+                  </div>
+                {/each}
+              </div>
             </div>
           </div>
-        </div>
-      {/each}
+        {/each}
+      </div>
     </div>
   </div>
+  <button class="absolute bottom-0 right-0 mr-5 mb-10">
+    <svg
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      class="w-12 h-12 stroke-current text-teal-700">
+      <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  </button>
 </main>
