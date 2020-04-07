10aa52b116bc9739b3bfa988b3fef01508f121cf
Javier Ruiz <j.ruiz@payvision.com>
2020-04-07 11:27:08 +0200

    Fix broken comment context menu

--- frontend/src/components/Card.svelte
+++ frontend/src/components/Card.svelte
@@ -42,19 +42,33 @@
 </script>
 
 <style>
-  #cardContainer:hover > #removeCardButton {
+  #cardContainer:hover #removeCardButton {
     display: block;
   }
 </style>
 
 <div
   id="cardContainer"
-  class="relative h-full flex flex-col py-8 bg-transparent-white rounded-lg
+  class="h-full flex flex-col py-8 bg-transparent-white rounded-lg
   border-gray-400 flex align-center w-full shadow-md overflow-y-scroll
   scrollable-container">
-  <div class="sticky bg-white -top-2 z-20 pl-8 pr-6">
+  <div class="relative sticky bg-white -top-2 z-20 pl-8 pr-6">
     <CardTitle title={card.title} updateTitle={onUpdateTitle} />
     <CommentBox onSubmit={addComment} />
+    <button
+      on:click|preventDefault={removeCard}
+      id="removeCardButton"
+      class="text-red-500 hover:text-red-700 absolute top-0 right-0 -mt-4 mr-1
+      z-20 hidden">
+      <svg fill="currentColor" viewBox="0 0 20 20" class="w-4 h-4 ">
+        <path
+          fill-rule="evenodd"
+          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0
+          002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012
+          0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+          clip-rule="evenodd" />
+      </svg>
+    </button>
   </div>
 
   <div class="mt-2 pl-8 pr-6">
@@ -64,19 +78,4 @@
       </div>
     {/each}
   </div>
-
-  <button
-    on:click|preventDefault={removeCard}
-    id="removeCardButton"
-    class="text-red-500 hover:text-red-700 absolute top-0 right-0 mt-2 mr-1 z-20
-    hidden">
-    <svg fill="currentColor" viewBox="0 0 20 20" class="w-4 h-4 ">
-      <path
-        fill-rule="evenodd"
-        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0
-        002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012
-        0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-        clip-rule="evenodd" />
-    </svg>
-  </button>
 </div>
