94c44bbf5fe736940abca3ddee1c9893715017da
Javier Ruiz <j.ruiz@payvision.com>
2020-04-11 15:45:36 +0200

    Fix creation of env variables in deploy script

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -10,7 +10,7 @@ fi
 PG_PASSWORD=$1
 DOCKER_TAG=$2
 
-echo "$PG_PASSWORD" >> .env
-echo "$DOCKER_TAG" >> .env
+echo "PG_PASSWORD=$PG_PASSWORD" >> .env
+echo "IMAGE_TAG=$DOCKER_TAG" >> .env
 
 docker-compose up -d
