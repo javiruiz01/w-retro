d3b15ba692665d98edcd2c54ec87f3270e4ade44
Javier Ruiz <j.ruiz@payvision.com>
2020-04-21 18:54:45 +0200

    Fix and use appropiate env variable name

--- database/preload.sh
+++ database/preload.sh
@@ -1,11 +1,10 @@
 echo "Waiting for sql to start"
-while [[ "$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Patata1. -Q "SELECT 'Hello world'" -h -1)" != *"Hello world"* ]]; do sleep 1; done
+while [[ "$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "SELECT 'Hello world'" -h -1)" != *"Hello world"* ]]; do sleep 1; done
 
 echo "Executing migration scripts"
 
 for file in migrations/*
 do
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Patata1. -i $file
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i $file
   echo "Executed migration: $file"
 done
-
