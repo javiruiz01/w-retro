8a7e4bf2113c687aca2e3f24313f01a4b1b028fe
Javier Ruiz <j.ruiz@payvision.com>
2020-05-03 11:03:00 +0200

    Add clip-path to background

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -14,6 +14,32 @@
   onMount(() => void hubClient.initHubConnection());
 </script>
 
+<style>
+  .main::before,
+  .main::after {
+    position: absolute;
+    content: '';
+    height: 100%;
+  }
+
+  .main::before {
+    background-color: #e2e8f0;
+    clip-path: polygon(0 50%, 0 100%, 100% 100%);
+    width: 70%;
+    bottom: 0;
+    left: 0;
+  }
+
+  .main::after {
+    background-color: #edf2f7;
+    clip-path: polygon(147% -30%, 0 100%, 100% 100%);
+    width: 80%;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+</style>
+
 <Navbar />
 <main class="main h-full">
   {#if ready}
