d016887bf4838d285b6022c305a436d39e5e5a57
Javier Ruiz <j.ruiz@payvision.com>
2020-08-10 20:26:25 +0200

    Fix a11y issue in empty svelte component

--- frontend/src/components/EmptyState.svelte
+++ frontend/src/components/EmptyState.svelte
@@ -3,7 +3,7 @@
     style="max-width: 400px;"
     class="w-full"
     src="assets/icons/add_content.svg"
-    alt="empty image" />
+    alt="empty" />
   <div class="flex flex-col justify-center items-center mt-6">
     <span class="text-md text-gray-700 font-semibold">
       Start adding some content!
